2c20aa885bfc85bf6075e196f4e900cb720a74f9
jongjam <jongjam64@gmail.com>
2023-10-19 16:27:07 +0900

    optimized quick sort and cleaned up code for less variables

--- quick_sort.py
+++ quick_sort.py
@@ -1,24 +1,45 @@
-def partition(nums, start, end):
-    i = start - 1 # Less
-    j = start # Greater 
+# def partition(nums, start, end):
+#     i = start - 1 # Less
+#     j = start # Greater 
     
-    pivot = end # It was ending one step early
+#     pivot = end # It was ending one step early
+    
+#     # We are checking to see if j is lesser than pivot
+#     # If so then we increment up
+    
+#     # If j is less than pivot, i increments and then 
+#     # the values at i and j swap
+#     j = start
+    
+#     while j < end :
+#         if nums[j] < nums[pivot] :
+#             i += 1
+#             nums[j], nums[i] = nums[i], nums[j]
+#         j += 1
+#     # I stops at the last lowest place of pivot... so pivot should rest at 1 next
+#     i += 1 
+#     nums[pivot], nums[i] = nums[i], nums[pivot] # Value at pivot
+  
+#     return i
+
+def partition(nums, start, end):
+    p = start # Greater 
+    pivot = nums[end] # It was ending one step early
     
     # We are checking to see if j is lesser than pivot
     # If so then we increment up
     
     # If j is less than pivot, i increments and then 
     # the values at i and j swap
-    j = start
+  
     
-    while j < end :
-        if nums[j] < nums[pivot] :
-            i += 1
-            nums[j], nums[i] = nums[i], nums[j]
-        j += 1
+    for i in range(start, end) :
+        if nums[i] < nums[pivot] :
+            
+            nums[p], nums[i] = nums[i], nums[p]
+            p += 1
     # I stops at the last lowest place of pivot... so pivot should rest at 1 next
-    i += 1 
-    nums[pivot], nums[i] = nums[i], nums[pivot] # Value at pivot
+    nums[p], nums[end] = nums[end], nums[p]
   
     return i
 
